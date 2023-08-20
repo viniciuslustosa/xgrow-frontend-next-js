e65880c3e5b0076b07f22caffeead4539280d986
Vinicius Lustosa <59030837+viniciuslustosa@users.noreply.github.com>
2023-08-20 20:49:05 -0300

    refactor: updated upload file input

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import styles from "./Upload.module.css";
 import { IconString } from "@/icons/types";
 import Icon from "@/icons";
+import Image from "next/image";
+import { Button } from "../Button";
+import { useRef, useState } from "react";
 
 type variants = "primary" | "secondary";
 type InputProps = {
@@ -16,15 +19,44 @@ const VariantMap = {
 };
 
 export const Upload: React.FunctionComponent<InputProps> = (props) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [selectedFile, setSelectedFile] = useState<File>();
+
+  const handleFileSelect = () => {
+    if(fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if(event.target.files) {
+      const selectedFile = event.target.files[0];
+      setSelectedFile(selectedFile);
+    }
+  };
+
   const { variant = "primary", icon, ...otherProps } = props;
   const className = [styles.root, VariantMap[variant]];
   return (
     <div className={className.join(" ")}>
-        <label className={styles.label}>{ props.label }</label>
-        <input type="file" className={styles.input} {...otherProps} />
-        { icon && (
-          <Icon name={icon} color={"#93BC1E"} size={18}></Icon>
-        )}
+        <label htmlFor="arquivo" className={styles.label}>{ props.label }</label>
+        <div className="text-center mx-auto">
+            <Image
+              width={100}
+              height={100}
+              src="/images/upload.svg"
+              alt="Preview" 
+              className="my-2" />
+        </div>
+        <input
+          ref={fileInputRef}
+          type="file"
+          onChange={handleFileChange}
+          className="hidden"
+          {...otherProps} />
+        <Button icon="FileUpload" variant="secondary" onClick={handleFileSelect}>
+          Upload
+        </Button>
     </div>
   );
 };
