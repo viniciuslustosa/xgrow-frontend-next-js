ca974b947f25fe8fbdfe7c795169d9a0eecdb7c2
Vinicius Lustosa <59030837+viniciuslustosa@users.noreply.github.com>
2023-08-20 11:45:01 -0300

    feat: auth context and config api

---
+++ src/config/api.ts
@@ -0,0 +1,41 @@
+"use client";
+
+import Cookie from 'js-cookie';
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://backend.challenge-la.xgrow.com/'
+})
+
+api.interceptors.response.use(
+  function (response: any) {
+    if(response.status > 226) {
+        //   modal.mixinToast("Houve um erro ao tentar se comunicar com o servidor.", 5000, 'error');
+      throw new Error("server-error");
+    }
+    return response;
+  },
+  function (error: { response: { status: any; }; }) {
+    let message = 'Houve um erro ao tentar se comunicar com o servidor'
+
+    switch (error.response.status) {
+      case 401:
+        message = 'Não autorizado!'
+        break;
+      case 404:
+        message = 'Não encontrado!'
+        break;
+    }
+
+    // modal.mixinToast(message, 4000, 'error', 'bottom');
+    throw error;
+  }
+);
+
+export default api;
+
+export const defaults = {
+    headers: {
+      Authorization: `Bearer ${Cookie.get('@App:accessToken')}`,
+    },
+};
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React, { createContext, useState, useEffect, useContext, ReactNode } from 'react';
+import { User } from '../@types/user';
+import Cookie from 'js-cookie';
+import api, { defaults } from '../config/api';
+import { useRouter } from 'next/navigation'
+
+interface AuthContextData {
+  signed: boolean;
+  user: Partial<User> | null;
+  Login(email: string, otp: string): Promise<void>;
+  Logout(): void;
+  SendToken(email: string): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+interface AuthProviderProps {
+    children: ReactNode;
+  }
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<Partial<User> | null>(null);
+  const [email, setEmail] = useState<User['email'] | null>(null);
+  const [signed, setSigned] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const storagedUser = Cookie.get('@App:user');
+    const storagedToken = Cookie.get('@App:accessToken');
+
+    if (storagedToken && storagedUser) {
+      setUser(JSON.parse(storagedUser));
+      defaults.headers.Authorization = `Bearer ${storagedToken}`;
+    }
+  }, []);
+
+  useEffect(() => {
+    setSigned(Boolean(user))
+  }, [user])
+
+  async function Login(email: User['email'], otp: string) {
+    try {
+      const response = await api.post('auth/login/otp', { email, otp });
+      
+      defaults.headers.Authorization = `Bearer ${response.data.accessToken}`;
+      
+      Cookie.set('@App:user', JSON.stringify({ email }));
+      Cookie.set('@App:accessToken', response.data.access_token);
+      router.push('/cursos')
+    } catch (error) {
+      router.push('/login')
+      console.log(error)
+    }
+  }
+
+  async function SendToken(email: string) {
+    try {
+      await api.post('auth/login', { email });
+      setUser({ email }); 
+      router.push('/login/token')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  function Logout() {
+    Cookie.remove('@App:user');
+    Cookie.remove('@App:accessToken');
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ signed, user, SendToken, Login, Logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
